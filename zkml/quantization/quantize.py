4b3f9d1bac6e9edec69b5ffcf0c92c5e40f1117c
Chichao Cheung <zhangzhichao@storswift.com>
2023-08-07 18:10:34 +0800

    feat: add quantize_not_clip and quantize_all function; fixed: dequantize result fixed

--- zkml/quantization/quantize.py
+++ zkml/quantization/quantize.py
@@ -36,59 +36,92 @@ def get_min(x: list):
     return x_min
 
 
-def calc_scale(x: list, target_type) -> float:
+def get_quantize_range(target_type):
     assert target_type in UINT or target_type in INT
     q_max = QUANTIZE_RANGE[target_type][Q_MAX]
     q_min = QUANTIZE_RANGE[target_type][Q_MIN]
+    return q_min, q_max
+
+
+def calc_scale(x: list, target_type) -> float:
+    q_min, q_max = get_quantize_range(target_type)
     # Calculate value range (denominator)
     x_range = get_max(x) - get_min(x)
     x_range = 1 if x_range == 0 else x_range
 
     # Calculate scale
-    scale = (q_max - q_min) / x_range
-    return scale
+    scale_molecule = q_max - q_min
+    scale_denominator = x_range
+    return scale_molecule, scale_denominator
 
 
-def calc_zero_point(x: list, scale: float, target_type: str) -> int:
-    assert target_type in UINT or target_type in INT
-    q_max = QUANTIZE_RANGE[target_type][Q_MAX]
-    q_min = QUANTIZE_RANGE[target_type][Q_MIN]
-
-    # zero_point = (-scale * get_min(x) + Q_MIN).round()
-    zero_point = (-scale * get_max(x) + q_max).__round__()
+def calc_zero_point(x: list, scale_molecule, scale_denominator, target_type: str) -> int:
+    q_min, q_max = get_quantize_range(target_type)
+    # zero_point = (-scale_molecule / scale_denominator * get_min(x) + q_min).round()
+    zero_point = (-scale_molecule / scale_denominator * get_max(x) + q_max).__round__()
     return zero_point
 
 
 def quantize(values: list, q_scale, q_zero_point: int, target_type: str):
     # detect input arg "target_type" in uint or int list
-    assert target_type in UINT or target_type in INT
-    q_max = QUANTIZE_RANGE[target_type][Q_MAX]
-    q_min = QUANTIZE_RANGE[target_type][Q_MIN]
+    q_min, q_max = get_quantize_range(target_type)
     x_quantize = []
     for value in values:
         x_quantize.append((value * q_scale + q_zero_point).__round__())
         # x_quantize = torch.clip((values * q_scale + q_zero_point).round(), min=Q_MIN, max=Q_MAX)
     x_quantize = torch.clip(torch.tensor(x_quantize), min=q_min, max=q_max)
 
-    return list(x_quantize)
+    return [int(x_ele) for x_ele in x_quantize]
+
+
+def quantize_not_clip(values: list, q_scale, q_zero_point: int):
+    # detect input arg "target_type" in uint or int list
+    x_quantize = []
+    for value in values:
+        x_quantize.append((value * q_scale + q_zero_point).__round__())
+        # x_quantize = torch.clip((values * q_scale + q_zero_point).round(), min=Q_MIN, max=Q_MAX)
+    x_quantize = torch.tensor(x_quantize)
+
+    return [int(x_ele) for x_ele in x_quantize]
+
 
 def dequantize(x_quantize: list, q_scale, q_zero_point: int, target_type: str):
     # detect input arg "target_type" in uint or int list
-    assert target_type in UINT or target_type in INT
-    q_max = QUANTIZE_RANGE[target_type][Q_MAX]
-    q_min = QUANTIZE_RANGE[target_type][Q_MIN]
+    _, _ = get_quantize_range(target_type)
     x_dequantize = []
     for ele in x_quantize:
         x_dequantize.append((ele - q_zero_point) / q_scale)
-    return [int(i) for i in quantize(x, scale, _zero_point, _type)]
+    return x_dequantize
+
+
+def quantize_all(values: list, target_type: str):
+    # quantize args (q_scale, q_zero_point) calculate by list
+    # detect input arg "target_type" in uint or int list
+    q_min, q_max = get_quantize_range(target_type)
+    scale_molecule, scale_denominator = calc_scale(values, target_type)
+    q_zero_point = calc_zero_point(values, scale_molecule, scale_denominator, target_type)
+    x_quantize = []
+    for value in values:
+        x_quantize.append((value * scale_molecule / scale_denominator + q_zero_point).__round__())
+        # x_quantize = torch.clip((values * q_scale + q_zero_point).round(), min=Q_MIN, max=Q_MAX)
+    x_quantize = torch.clip(torch.tensor(x_quantize), min=q_min, max=q_max)
+    x_quantize = [int(x_ele) for x_ele in x_quantize]
+    return x_quantize, scale_molecule, scale_denominator, q_zero_point, get_max(values), get_min(values)
 
 
 if __name__ == '__main__':
     x = [-3.0, 0.1, 3.2, -3.0, -0.3, 3.2, -2.0, 0.2, 2.0, -1.0, 0.1, 1.0, -3.2, 1.0, 3.0]
     _type = "uint8"
-    scale = calc_scale(x, _type)
-    _zero_point = calc_zero_point(x, scale, _type)
-    print(scale)
-    print(_zero_point)
+    _f_min, _f_max = get_max(x), get_min(x)
+    print(_f_max, _f_min)
+    _scale_molecule, _scale_denominator = calc_scale(x, _type)
+    _zero_point = calc_zero_point(x, _scale_molecule, _scale_denominator, _type)
+    scale = _scale_molecule / _scale_denominator
+    print("scale", scale)
+    print("zero_point", _zero_point)
     print(quantize(x, scale, _zero_point, _type))
-    print(dequantize(quantize(x, scale, _zero_point, _type), scale, _zero_point, _type))
+    print(dequantize(
+        quantize(x, scale, _zero_point, _type),
+        scale,
+        _zero_point,
+        _type))
